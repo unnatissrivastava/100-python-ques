b02b22a297a2d1a293d7bbe9fb83a8dfcbbe765c
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-07 17:31:38 +0530

    Update celsius-to-fahrenheit.py

--- celsius-to-fahrenheit.py
+++ celsius-to-fahrenheit.py
@@ -1,3 +1,9 @@
-celsius = int(input("enter temperature in celsius"))
-fahrenheit = (celsius * 9/5) + 32
-print("the converted temperature from f to c is", fahrenheit)
+age1 = int(input("enter 1st age: "))
+age2 = int(input("enter 2nd age: "))
+age3 = int(input("enter 3rd age: "))
+if age1 > age2 and age1 > age3:
+    print("age1 is the oldest age")
+elif age2>age3 and age2 > age1:
+    print("age2 is the oldest age")
+else:
+    print("age3 is the oldest age")
