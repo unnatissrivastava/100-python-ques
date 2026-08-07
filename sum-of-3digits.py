df1f880c921835d70dcebb6aa9402b93b34569f3
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-07 18:11:40 +0530

    Update sum-of-3digits.py

--- sum-of-3digits.py
+++ sum-of-3digits.py
@@ -1,1 +1,10 @@
+number = int(input("Enter a number: "))
+total_sum = 0
+
+while number > 0:
+    digit = number % 10
+    total_sum = total_sum + digit
+    number = number // 10
+
+print("Sum of digits is:", total_sum)
 
