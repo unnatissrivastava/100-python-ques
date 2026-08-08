3ac289c39c17a058009c1d0c7e8d74bbe902b149
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-08 17:29:58 +0530

    Update reversing-4digit-number.py

--- reversing-4digit-number.py
+++ reversing-4digit-number.py
@@ -1,1 +1,9 @@
+number = int(input("enter a number: "))
+reverse = 0
 
+while number> 0:
+    digit = number % 10
+    reverse = reverse*10 + digit
+    number = number // 10
+    
+print("reversed number is", reverse)
