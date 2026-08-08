b372c3ceb22352e59043f27cdedd4fd5d98b3257
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-08 22:24:00 +0530

    Update forming-triangle.py

--- forming-triangle.py
+++ forming-triangle.py
@@ -1,1 +1,8 @@
+x = int(input("enter angle 1: "))
+y = int(input("enter angle 2: "))
+z = int(input("enter angle 3: "))
 
+if x + y + z == 180:
+    print("it forms a triangle!!")
+else:
+    print("doesn't form a triangle")
