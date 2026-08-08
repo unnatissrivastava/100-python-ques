a918d17664e73c6716cdaeddb958195401073d2e
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-08 22:37:25 +0530

    Update simple-interest.py

--- simple-interest.py
+++ simple-interest.py
@@ -1,1 +1,5 @@
-
+p = float(input("Enter the Value of Principle: "))
+r = float(input("Enter the ROI: "))
+t = float(input("Enter the time period: "))
+simple_interest = p * r * t/100
+print("The Calculated Simple Interest is - ", simple_interest)
