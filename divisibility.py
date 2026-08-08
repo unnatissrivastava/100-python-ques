bc907b9b5be6341973985a23b03a390fbde10eeb
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-08 23:09:52 +0530

    Update divisibility.py

--- divisibility.py
+++ divisibility.py
@@ -1,1 +1,7 @@
-
+num = int(input("enter a number:"))
+if num % 3 == 0:
+    print("divisible by 3")
+elif num % 6 == 0:
+    print("divisible by 6")
+else:
+    print("divisible by neither of the two numbers")
