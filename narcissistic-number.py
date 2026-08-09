78975d5ba455a3b3438966822a07277bbe94ba54
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-09 23:01:42 +0530

    Update narcissistic-number.py

--- narcissistic-number.py
+++ narcissistic-number.py
@@ -1,1 +1,14 @@
+number = int(input("Enter a 4 digit number: "))
+original = number
+num_digits = len(str(number))
 
+total_sum = 0
+while number > 0:
+    digit = number % 10
+    total_sum = total_sum + digit ** num_digits
+    number = number // 10
+
+if total_sum == original:
+    print("It is a Narcissistic number")
+else:
+    print("It is not a Narcissistic number")
