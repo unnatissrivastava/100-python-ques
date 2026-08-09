63460e6e4558f08f063db43f49751ce989c2b579
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-09 22:51:24 +0530

    Update armstrong-number.py

--- armstrong-number.py
+++ armstrong-number.py
@@ -1,1 +1,15 @@
+number = int(input("Enter a number: "))
+original = number         
+num_digits = len(str(number))  
 
+total_sum = 0
+
+while number > 0:
+    digit = number % 10
+    total_sum = total_sum + digit ** num_digits  
+    number = number // 10
+
+if total_sum == original:
+    print("It is an Armstrong number")
+else:
+    print("It is not an Armstrong number")
