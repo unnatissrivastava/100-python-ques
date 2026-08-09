459e8200d3d43b9e3ad1262d582b1bc0ed21bf44
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-09 22:41:55 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,1 +1,13 @@
+t = float(input("What is the temperature?: "))
+h = float(input("Enter the amount of humidity present: "))
 
+if t >= 30 and h >= 90:
+    print("Weather is HOT AND HUMID")
+elif t >= 30 and h < 90:
+    print("Weather is HOT")
+elif t<30 and h >= 90:
+    print("Weather is COOL AND HUMID")
+elif t<30 and h <90:
+    print("Weather is COOL")
+else:
+    print("INVALID INFORMATION")
