405a83c8e4ed3dcac961346af91b88c1eaa022d3
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-10 23:28:19 +0530

    Update dogs-chicken-legs-heads.py

--- dogs-chicken-legs-heads.py
+++ dogs-chicken-legs-heads.py
@@ -1,1 +1,8 @@
+total_heads = int(input("Enter total heads: "))
+total_legs = int(input("Enter total legs: "))
 
+dogs = (total_legs - 2 * total_heads) / 2
+chickens = total_heads - dogs
+
+print("Dogs:", dogs)
+print("Chickens:", chickens)
