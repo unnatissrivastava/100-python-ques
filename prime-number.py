444feedc2a53950bb4cf722c64a9742656809f3a
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-10 23:46:19 +0530

    Update prime-number.py

--- prime-number.py
+++ prime-number.py
@@ -1,1 +1,11 @@
+n = int(input("Enter a number: "))
+is_prime = True
 
+for i in range(2, n):
+    if n % i == 0:
+        is_prime = False
+
+if is_prime:
+    print("Prime")
+else:
+    print("Not Prime")
