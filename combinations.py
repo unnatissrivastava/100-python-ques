d992a095ca7ec04ed11eff6cc698970637dfa899
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-13 22:10:51 +0530

    Update combinations.py

--- combinations.py
+++ combinations.py
@@ -1,1 +1,8 @@
+numbers = [1, 2, 3, 4]
 
+for a in numbers:
+    for b in numbers:
+        for c in numbers:
+            for d in numbers:
+                if a != b and a != c and a != d and b != c and b != d and c != d:
+                    print(a, b, c, d)
